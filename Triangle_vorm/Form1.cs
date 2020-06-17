1d904ac5af05dbffe411904097e35d5d2ed1852a
FIRST_ILJA LAST_FILATOV <iljafilatov@example.com>
2020-06-18 01:26:22 +0300

    Add project files.

---
+++ Triangle_vorm/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Triangle_vorm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Run_button_DoubleClick(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count > 0)
+            {
+                listView1.Items.Clear();//Очистка формы от значений
+
+
+            }
+            if (txtA.Text.Length > 0 && txtB.Text.Length > 0 && txtC.Text.Length > 0) // Проверка на введённые символы
+            {
+                double a, b, c;
+                a = Convert.ToDouble(txtA.Text);
+                b = Convert.ToDouble(txtB.Text);
+                c = Convert.ToDouble(txtC.Text);
+                Triangle triangle = new Triangle(a, b, c);
+                listView1.Items.Add("Сторона a");
+                listView1.Items.Add("Сторона b");
+                listView1.Items.Add("Сторона c");
+                listView1.Items.Add("Высота");
+                listView1.Items.Add("Периметр");
+                listView1.Items.Add("Площадь");
+                listView1.Items.Add("Существует?");
+                listView1.Items.Add("Спецификатор");
+                listView1.Items[0].SubItems.Add(triangle.outputA());
+                listView1.Items[1].SubItems.Add(triangle.outputB());
+                listView1.Items[2].SubItems.Add(triangle.outputC());
+                listView1.Items[3].SubItems.Add(Convert.ToString(triangle.Height()));//Высота
+                listView1.Items[4].SubItems.Add(Convert.ToString(triangle.Perimeter()));
+                listView1.Items[5].SubItems.Add(Convert.ToString(triangle.Surface()));
+
+                if (triangle.ExistTriangle) { listView1.Items[6].SubItems.Add("Cуществует"); }
+                if (a == b && b == c && c == b) { listView1.Items[7].SubItems.Add("Равносторонний"); pictureBox1.Image = Properties.Resources._1; }
+                else if (a == b || b == c || c == a) { listView1.Items[7].SubItems.Add("Равнобедренный"); pictureBox1.Image = Properties.Resources._2; }
+                else if (a != b && a != c && b != c) { listView1.Items[7].SubItems.Add("Разносторонний"); pictureBox1.Image = Properties.Resources._3; }
+                else listView1.Items[6].SubItems.Add("Не существует");
+
+
+
+
+
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Triangle_vorm/Triangle.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_vorm
+
+{
+    class Triangle
+    {
+        public double a;// первая сторона
+        public double b;// вторая сторона
+        public double c;// третья сторона
+        public double h;// Высота
+        public Triangle(double A, double B, double C)
+        {
+            a = A;
+            b = B;
+            c = C;
+        }
+        public Triangle(double A, double B, double C, double H)
+        {
+            a = A;
+            b = B;
+            c = C;
+            h = H;
+        }
+        public Triangle() // Конструктор, создаст без указания переменов
+        {
+        }
+
+        public string outputA()
+        {
+            return Convert.ToString(a);
+        }
+        public string outputB()
+        {
+            return Convert.ToString(b);
+        }
+        public string outputC()
+        {
+            return Convert.ToString(c);
+        }
+        public double Perimeter()
+        {
+            double p = 0;
+            p = a + b + c;
+            return p;
+        }
+        public double perimeterforh()
+        {
+            return Perimeter() * 0.5; // Формула для вычисления
+        }
+        public double Height() // Вычисление высоту треугольника
+        {
+
+            double p = perimeterforh();
+            double h = 2 * Math.Sqrt(p * (p - a) * (p - b) * (p - c)) / a; // Формула для вычисления
+            return h; // Выводим высоту
+
+        }
+        public double Surface()
+        {
+            double s = 0;
+            double p = 0;
+            p = (a + b + c) / 2;
+            s = Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+            return s;
+        }
+        public double GetSetA
+        {
+            get
+            { return a; }
+            set
+            { a = value; }
+        }
+        public double GetSetB
+        {
+            get
+            { return b; }
+            set
+            { b = value; }
+        }
+        public double GetSetC
+        {
+            get
+            { return c; }
+            set
+            { c = value; }
+        }
+        // public double GetSetP
+        // {
+        //     get
+        //     { return p; }
+        //     set
+        //     { p = value; }
+        // }
+        public bool ExistTriangle
+        {
+            get
+            {
+                if ((a > b + c) && (b > a + c) && (c > a + b))
+                    return false;
+                else return true;
+            }
+        }
+    }
+}
+ 
